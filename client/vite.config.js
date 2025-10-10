0e6ed9a9acfa8d71e727046cd3be88d8562ae93b
Hee Seo Kim <hisounni@ewhain.net>
2025-10-10 19:22:02 +0900

    캐싱관련

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        // 파일명에 해시 추가하여 캐싱 문제 방지
+        entryFileNames: `assets/[name].[hash].js`,
+        chunkFileNames: `assets/[name].[hash].js`,
+        assetFileNames: `assets/[name].[hash].[ext]`
+      }
+    }
+  }
 })
