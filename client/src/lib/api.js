729e355ee608bae04b8f3d3605139018ef9e7953
Hee Seo Kim <hisounni@ewhain.net>
2025-10-18 03:48:07 +0900

    Revert "Update auth.js"
    This reverts commit 6ef74d517c2538ed51b3b65d8f064b6edec62b2a.

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -18,28 +18,4 @@ api.interceptors.request.use((config) => {
   return Promise.reject(error);
 });
 
-// 응답 인터셉터: 401 에러 시 자동으로 로그인 페이지로 리다이렉트
-api.interceptors.response.use(
-  (response) => {
-    // 성공 응답은 그대로 반환
-    return response;
-  },
-  (error) => {
-    // 401 Unauthorized 에러 처리
-    if (error.response && error.response.status === 401) {
-      // 토큰 삭제
-      localStorage.removeItem('ewhaian_token');
-      sessionStorage.removeItem('ewhaian_token');
-      
-      // 현재 로그인 페이지가 아닌 경우에만 리다이렉트
-      if (window.location.pathname !== '/login' && window.location.pathname !== '/register') {
-        // 로그인 페이지로 리다이렉트
-        window.location.href = '/login';
-      }
-    }
-    
-    return Promise.reject(error);
-  }
-);
-
 export default api;   
