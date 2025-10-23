8f24b2e453b6d0883588222bcf00eb13e15ab3cc
Hee Seo Kim <hisounni@ewhain.net>
2025-10-23 20:04:56 +0900

    Revert "Update Banner.jsx"
    This reverts commit a5fc9ed9e7bcdf19fc9607a68b2bf0e315b16c6f.

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import { useState, useEffect } from 'react';
 import bannerImage from '../assets/images/banner.png';
 
 const BannerContainer = styled.div`
@@ -27,8 +26,6 @@ const BannerImage = styled.img`
   height: auto;
   object-fit: contain;
   max-width: 100%;
-  opacity: ${props => props.loaded ? 1 : 0};
-  transition: opacity 0.3s ease-in-out;
   
   @media (min-width: 768px) {
     max-width: 600px;
@@ -36,74 +33,20 @@ const BannerImage = styled.img`
   
   @media (min-width: 1024px) {
     max-width: 800px;
-  }
-`;
-
-const LoadingPlaceholder = styled.div`
-  width: 100%;
-  height: 200px;
-  background-color: #f0f0f0;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  color: #666;
-  font-size: 14px;
-  
-  @media (min-width: 768px) {
-    max-width: 600px;
-    height: 150px;
-  }
-  
-  @media (min-width: 1024px) {
-    max-width: 800px;
-    height: 200px;
   }
 `;
 
 const Banner = () => {
-  const [imageLoaded, setImageLoaded] = useState(false);
-  const [imageError, setImageError] = useState(false);
-
   const handleBannerClick = () => {
     window.open('https://ewhadam-padlet.com/', '_blank');
   };
 
-  const handleImageLoad = () => {
-    setImageLoaded(true);
-  };
-
-  const handleImageError = () => {
-    setImageError(true);
-  };
-
-  // 이미지 preload
-  useEffect(() => {
-    const img = new Image();
-    img.onload = handleImageLoad;
-    img.onerror = handleImageError;
-    img.src = bannerImage;
-  }, []);
-
   return (
     <BannerContainer>
       <BannerLink onClick={handleBannerClick}>
-        {!imageLoaded && !imageError && (
-          <LoadingPlaceholder>
-            배너 로딩 중...
-          </LoadingPlaceholder>
-        )}
-        {imageError && (
-          <LoadingPlaceholder>
-            배너를 불러올 수 없습니다
-          </LoadingPlaceholder>
-        )}
         <BannerImage 
           src={bannerImage} 
           alt="11월 행사 배너"
-          loaded={imageLoaded}
-          onLoad={handleImageLoad}
-          onError={handleImageError}
-          style={{ display: imageLoaded ? 'block' : 'none' }}
         />
       </BannerLink>
     </BannerContainer>
